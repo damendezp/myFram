738de063fc4dd2b3ce0bd1b74c862c29183f789a
Diego Andres Mendez Pastrana <ing.diego.mendez@gmail.com>
2014-08-25 09:14:22 -0500

    Primeros archivos subidos

---
+++ application/Config.php
@@ -0,0 +1,9 @@
+<?php
+define('BASE_URL','http://localhost/mvc/');
+define('DEFAULT_CONTROLLER','index');
+define('DEFAULT_LAYOUT','default');
+
+define('APP_NAME','Mi Framework');
+define('APP_ESLOGAN','mi primer framework php mvc');
+define('APP_COMPANY','damendez');
+?>
---
+++ views/layout/default/footer.php
@@ -0,0 +1,6 @@
+<div id="footer">
+    Copyright &COPY; 2014 <?php echo APP_COMPANY; ?>
+</div>
+</div>
+</body>
+</html>
